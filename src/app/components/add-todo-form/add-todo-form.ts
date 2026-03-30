b1f5e1b5b357bce64c123a2b9fc48d3f089571e1
Adrian Kowal <adriankowal.post@gmail.com>
2026-03-30 11:20:33 +0200

    Add components, models and services

---
+++ src/app/components/add-todo-form/add-todo-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo-form',
+  imports: [],
+  templateUrl: './add-todo-form.html',
+  styleUrl: './add-todo-form.css',
+})
+export class AddTodoForm {}
